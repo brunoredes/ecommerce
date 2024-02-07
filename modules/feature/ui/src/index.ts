4f9f0c0f400d5a977c9757f8f609aaf9f4834ac0
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-02-07 20:12:34 -0300

    feat(header): add component

--- modules/feature/ui/src/index.ts
+++ modules/feature/ui/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/ui.module';
+
+export * from './lib/modules/feature/ui/src/lib/header.component';
--- modules/feature/ui/src/lib/ui.module.ts
+++ modules/feature/ui/src/lib/ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './modules/feature/ui/src/lib/header.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
 })
 export class UiModule {}
