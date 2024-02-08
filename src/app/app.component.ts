bc26c6fa2179d2964a28b86c26d7a31b05856c16
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-02-07 21:04:53 -0300

    feat(UI): add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiModule } from '@ecommerce/ui';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, UiModule],
   selector: 'ecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
