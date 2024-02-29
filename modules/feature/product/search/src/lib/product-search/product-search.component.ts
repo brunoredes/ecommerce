2a84bc490c7e835b9f25547393ab0fbe7630d230
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-02-29 20:34:04 -0300

    feat(product-search): add lib

---
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ecommerce-product-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-search.component.html',
+  styleUrl: './product-search.component.scss',
+})
+export class ProductSearchComponent {}
