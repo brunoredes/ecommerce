9f43c9ce142f2f97621a438d86779cc1f1f52805
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-02-29 21:10:27 -0300

    feat(product-search): add search to header

--- modules/feature/product/search/src/lib/product-search/product-search.component.ts
+++ modules/feature/product/search/src/lib/product-search/product-search.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { productsMock } from '@ecommerce/product-data-access';
 
 @Component({
   selector: 'ecommerce-product-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
   templateUrl: './product-search.component.html',
   styleUrl: './product-search.component.scss',
 })
-export class ProductSearchComponent {}
+export class ProductSearchComponent {
+  control = new FormControl({
+    value: '',
+    disabled: false,
+  });
+  products = productsMock;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductSearchComponent } from '@ecommerce/product-search';
 import { UiModule } from '@ecommerce/ui';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, UiModule],
+  imports: [RouterModule, UiModule, ProductSearchComponent],
   selector: 'ecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
