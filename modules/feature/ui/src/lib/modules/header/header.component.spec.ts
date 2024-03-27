9983307ee93cd3a2313f6d31b50a132c5a761623
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-03-27 20:41:29 -0300

    feat(header): add link to home

--- modules/feature/ui/src/lib/modules/header/header.component.spec.ts
+++ modules/feature/ui/src/lib/modules/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { input } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
 const mockTitle = 'Mock Title';
 
@@ -11,6 +12,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
@@ -31,5 +33,10 @@ describe('HeaderComponent', () => {
     component.title = input('Another title');
     fixture.detectChanges();
     expect(title.textContent).toBe('Another title');
+  });
+
+  it('should redirect to "/" when logo is is clicked', () => {
+    const anchor: HTMLAnchorElement = fixture.nativeElement.querySelector('a');
+    expect(anchor.getAttribute('href')).toBe('/');
   });
 });
