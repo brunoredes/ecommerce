77a7f5d504562d2cd32b11b2109cc20ce87b49b0
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-05-02 21:15:50 -0300

    refactor(app): change routertestingmodule to provideRouter

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, RouterTestingModule, HttpClientTestingModule],
+      imports: [AppComponent, HttpClientTestingModule],
+      providers: [provideRouter(appRoutes)],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductSearchComponent } from '@ecommerce/product-search';
 import { UiModule } from '@ecommerce/ui';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @Component({
   standalone: true,
-  imports: [RouterModule, UiModule, ProductSearchComponent],
+  imports: [RouterModule, UiModule, ProductSearchComponent, MatSnackBarModule],
   selector: 'ecommerce-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
