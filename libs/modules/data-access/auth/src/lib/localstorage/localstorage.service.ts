659c54e3aa7cbda2f5a11a88002849509c33bc2b
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-08-06 22:34:36 -0300

    feat(localstorage): add service

---
+++ libs/modules/data-access/auth/src/lib/localstorage/localstorage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalstorageService {
+  setItem(key: string, value: string): void {
+    localStorage.setItem(key, value);
+  }
+
+  getItem(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+}
