848ddc4bc2e715798baadda83f23ef17b257777c
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-08-07 15:56:43 -0300

    chore(config): add interceptor provider

--- apps/ecommerce-admin-panel/src/app/app.config.ts
+++ apps/ecommerce-admin-panel/src/app/app.config.ts
@@ -1,17 +1,18 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection
+  provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { httpRequestInterceptor } from './interceptors/http-request.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(appRoutes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([httpRequestInterceptor])),
     provideAnimationsAsync(),
   ],
 };
