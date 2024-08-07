03594b1f2781b806f875e23690573bcbeed17369
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-08-07 13:05:53 -0300

    test(email-form-state): add tests

--- libs/modules/data-access/auth/src/lib/state/email-form-state.service.spec.ts
+++ libs/modules/data-access/auth/src/lib/state/email-form-state.service.spec.ts
@@ -13,4 +13,22 @@ describe('EmailFormStateService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should initialize email with an empty string', () => {
+    expect(service.email()).toBe('');
+  });
+
+  it('should update the email correctly', () => {
+    const testEmail = 'test@example.com';
+    service.setEmail(testEmail);
+    expect(service.email()).toBe(testEmail);
+  });
+
+  it('should return the email as readonly', () => {
+    const testEmail = 'readonly@example.com';
+    service.setEmail(testEmail);
+    const emailSignal = service.email;
+    expect(emailSignal).toBeDefined();
+    expect(emailSignal()).toBe(testEmail);
+  });
 });
