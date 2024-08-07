b9bf323223681986cd2369cbfc1782bd0d63bc2e
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-08-06 22:21:08 -0300

    aaaaaaaaaaaaaaaaaaaa

--- apps/ecommerce-admin-panel/src/app/app.component.ts
+++ apps/ecommerce-admin-panel/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UiModule } from '@ecommerce/ui';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, UiModule],
+  imports: [
+    RouterModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    UiModule,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'ecommerce-admin-panel';
