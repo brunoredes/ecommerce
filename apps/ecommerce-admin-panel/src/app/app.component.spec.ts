d2cccc532eea2183739b8b2e76fcfa18f712049d
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-08-10 11:50:30 -0300

    test(app): add suit test

--- apps/ecommerce-admin-panel/src/app/app.component.spec.ts
+++ apps/ecommerce-admin-panel/src/app/app.component.spec.ts
@@ -1,27 +1,94 @@
-import { TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { By } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter, RouterModule } from '@angular/router';
+import { AuthService } from '@ecommerce/auth-data-access';
+import { UiModule } from '@ecommerce/ui';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let authService: AuthService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterModule.forRoot([])],
-    }).compileComponents();
+      imports: [
+        CommonModule,
+        RouterModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSidenavModule,
+        UiModule,
+      ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            isAuthenticated: jest.fn(),
+            logout: jest.fn(),
+          },
+        },
+        provideRouter([]),
+        provideAnimationsAsync(),
+      ],
+    })
+      .overrideComponent(AppComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should display actions template when authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(true);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome ecommerce-admin-panel',
+    const buttonElement = fixture.debugElement.query(
+      By.css('button[title="Sair"]'),
     );
+    expect(buttonElement).toBeTruthy();
   });
-
-  it(`should have as title 'ecommerce-admin-panel'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ecommerce-admin-panel');
+  it('should display empty template when not authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(false);
+    fixture.detectChanges();
+    const buttonElement = fixture.debugElement.query(
+      By.css('button[title="Sair"]'),
+    );
+    expect(buttonElement).toBeFalsy();
+  });
+  it('should call logout method on button click', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(true);
+    fixture.detectChanges();
+    const buttonElement = fixture.debugElement.query(
+      By.css('button[title="Sair"]'),
+    );
+    buttonElement.nativeElement.click();
+    expect(authService.logout).toHaveBeenCalled();
+  });
+  it('should display navigation template when authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(true);
+    fixture.detectChanges();
+    const navElement = fixture.debugElement.query(
+      By.css('a[routerLink="/home"]'),
+    );
+    expect(navElement).toBeTruthy();
+  });
+  it('should display login template when not authenticated', () => {
+    jest.spyOn(authService, 'isAuthenticated').mockReturnValue(false);
+    fixture.detectChanges();
+    const navElement = fixture.debugElement.query(
+      By.css('a[routerLink="/home"]'),
+    );
+    expect(navElement).toBeFalsy();
   });
 });
