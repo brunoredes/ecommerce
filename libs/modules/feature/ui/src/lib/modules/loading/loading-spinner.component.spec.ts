f377b11d50b67e688ff0ad4eb183f340c9975d74
Bruno Donatelli <bruno.donatelli17@gmail.com>
2024-08-10 11:46:49 -0300

    test(loading-spinner): add test suit

--- libs/modules/feature/ui/src/lib/modules/loading/loading-spinner.component.spec.ts
+++ libs/modules/feature/ui/src/lib/modules/loading/loading-spinner.component.spec.ts
@@ -1,21 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoadingSpinnerComponent } from './loading-spinner.component';
+import { LoadingService } from '@ecommerce/admin-data-access';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('LoadingSpinnerComponent', () => {
   let component: LoadingSpinnerComponent;
   let fixture: ComponentFixture<LoadingSpinnerComponent>;
+  let loadingService: LoadingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoadingSpinnerComponent],
-    }).compileComponents();
+      imports: [
+        CommonModule,
+        MatProgressSpinnerModule,
+      ],
+      declarations: [],
+      providers: [
+        {
+          provide: LoadingService,
+          useValue: {
+            loading: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideComponent(LoadingSpinnerComponent, {
+        set: { changeDetection: ChangeDetectionStrategy.Default },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoadingSpinnerComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    loadingService = TestBed.inject(LoadingService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display spinner when loading is true', () => {
+    jest.spyOn(loadingService, 'loading').mockReturnValue(true);
+    fixture.detectChanges();
+
+    const spinnerElement = fixture.debugElement.query(By.css('mat-spinner'));
+    const loadingText = fixture.debugElement.query(By.css('span'));
+
+    expect(spinnerElement).toBeTruthy();
+    expect(loadingText.nativeElement.textContent).toContain('Carregando');
+  });
+
+  it('should not display spinner when loading is false', () => {
+    jest.spyOn(loadingService, 'loading').mockReturnValue(false);
+    fixture.detectChanges();
+
+    const spinnerElement = fixture.debugElement.query(By.css('mat-spinner'));
+    const loadingText = fixture.debugElement.query(By.css('span'));
+
+    expect(spinnerElement).toBeFalsy();
+    expect(loadingText).toBeFalsy();
   });
 });
